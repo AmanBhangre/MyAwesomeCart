e831db2fdcf4fba7265aefffbbef4145a15a421a
AmanBhangre <amanbhangre511@gmail.com>
2021-08-14 20:25:47 +0530

    Added some changes

--- mac/settings.py
+++ mac/settings.py
@@ -121,24 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # managing media
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-
-# Extra places for collectstatic to find static files.
-MEDIAFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
-)
+MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
